b303ae68ca4156dc0641523da2f284958a50e888
Amulya Khurana <51925192+amulyakhurana@users.noreply.github.com>
2021-05-21 10:00:01 -0400

    Added comments

--- print_manipulation.py
+++ print_manipulation.py
@@ -1,3 +1,11 @@
+""""
+
+Code for keyboard manipulation to read user data and interact with the editor to print a label.
+
+Authors: Aparajito Saha and Amulya Khurana
+
+""""
+
 import time
 import pyautogui
 import csv 
@@ -5,7 +13,8 @@ import csv
 def print_label(id):
     fields = []
     rows = []
-
+       
+    #read student data from a CSV
     data = open("sample_student_data_read.csv", "r")
     csvreader = csv.reader(data)
     # extracting field names through first row
@@ -17,9 +26,11 @@ def print_label(id):
 
     ind = 0     
     time.sleep(5)
-
+    
+    #go through each row and compare the student ID
     for row in rows:
         if row[0] != str(id):
+            #move down to required row
             pyautogui.press('down')
             ind = rows.index(row)
             ind += 1
@@ -28,11 +39,13 @@ def print_label(id):
                 print(col)
             break
 
+    #CTRL-P to print the label and confirm
     time.sleep(5)
     pyautogui.hotkey('ctrl', 'p')
     time.sleep(3)
     pyautogui.hotkey('enter')
     time.sleep(3)
-
+    
+    #go back to the top of the CSV
     for i in range(ind):
         pyautogui.press('up')
